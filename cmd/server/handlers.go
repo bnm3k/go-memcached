5a0a04752c46e08a9bf52df561cc48d157c27fa5
nagamocha3000 <nagamocha@gmail.com>
2020-01-11 07:54:00 +0300

    add get vars from url

--- cmd/server/handlers.go
+++ cmd/server/handlers.go
@@ -24,68 +24,80 @@ func (api *httpAPI) getValue(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(fmt.Sprintf("val for key %d", id)))
 }
 
+func getStdParams(r *http.Request) (string, string, string) {
+	key := r.URL.Query().Get(":key")
+	val := r.URL.Query().Get("val")
+	exptimeStr := r.URL.Query().Get("exp")
+	if exptimeStr == "" {
+		exptimeStr = "-1"
+	}
+	return key, val, exptimeStr
+}
+
 func (api *httpAPI) handleSet(w http.ResponseWriter, r *http.Request) {
-	var key, val, exptimeStr string
+	key, val, exptimeStr := getStdParams(r)
 	reply := api.cache.Set(key, val, exptimeStr)
 	//return only reply
 	w.Write([]byte(reply))
 }
 
 func (api *httpAPI) handleAdd(w http.ResponseWriter, r *http.Request) {
-	var key, val, exptimeStr string
+	key, val, exptimeStr := getStdParams(r)
 	reply := api.cache.Add(key, val, exptimeStr)
 	//return only reply
 	w.Write([]byte(reply))
 }
 
 func (api *httpAPI) handleReplace(w http.ResponseWriter, r *http.Request) {
-	var key, val, exptimeStr string
+	key, val, exptimeStr := getStdParams(r)
 	reply := api.cache.Replace(key, val, exptimeStr)
 	//return only reply
 	w.Write([]byte(reply))
 }
 
 func (api *httpAPI) handleAppend(w http.ResponseWriter, r *http.Request) {
-	var key, val, exptimeStr string
+	key, val, exptimeStr := getStdParams(r)
 	reply := api.cache.Append(key, val, exptimeStr)
 	//return only reply
 	w.Write([]byte(reply))
 }
 
 func (api *httpAPI) handlePrepend(w http.ResponseWriter, r *http.Request) {
-	var key, val, exptimeStr string
+	key, val, exptimeStr := getStdParams(r)
 	reply := api.cache.Prepend(key, val, exptimeStr)
 	//return only reply
 	w.Write([]byte(reply))
 }
 
 func (api *httpAPI) handleIncrement(w http.ResponseWriter, r *http.Request) {
-	var key, numStr string
+	key := r.URL.Query().Get(":key")
+	numStr := r.URL.Query().Get("num")
 	reply := api.cache.Increment(key, numStr)
 	w.Write([]byte(reply))
 }
 
 func (api *httpAPI) handleDecrement(w http.ResponseWriter, r *http.Request) {
-	var key, numStr string
+	key := r.URL.Query().Get(":key")
+	numStr := r.URL.Query().Get("num")
 	reply := api.cache.Decrement(key, numStr)
 	w.Write([]byte(reply))
 }
 
 func (api *httpAPI) handleCompareAndSwap(w http.ResponseWriter, r *http.Request) {
-	var key, val, exptimeStr string
+	key, val, exptimeStr := getStdParams(r)
 	reply := api.cache.CompareAndSwap(key, val, exptimeStr, "")
 	w.Write([]byte(reply))
 }
 
 func (api *httpAPI) handleGet(w http.ResponseWriter, r *http.Request) {
-	var key string
+	key := r.URL.Query().Get(":key")
 	val, reply := api.cache.Get(key)
 	w.Write([]byte(reply))
 	w.Write([]byte(val))
 }
 
 func (api *httpAPI) handleGetEntryPlusToken(w http.ResponseWriter, r *http.Request) {
-	var key string
+	key := r.URL.Query().Get(":key")
 	val, reply, token := api.cache.GetEntryPlusToken(key)
 	w.Write([]byte(reply))
 	w.Write([]byte(val))
@@ -93,7 +105,7 @@ func (api *httpAPI) handleGetEntryPlusToken(w http.ResponseWriter, r *http.Reque
 }
 
 func (api *httpAPI) handleDelete(w http.ResponseWriter, r *http.Request) {
-	var key string
+	key := r.URL.Query().Get(":key")
 	reply := api.cache.Delete(key)
 	w.Write([]byte(reply))
 }
