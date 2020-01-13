2b30eebdedfe6863cd7cb0829ca14138a721c1b6
nagamocha3000 <nagamocha@gmail.com>
2020-01-13 19:26:19 +0300

    add cache config fields

--- cmd/server/config.go
+++ cmd/server/config.go
@@ -3,13 +3,17 @@ package main
 import "flag"
 
 type config struct {
-	addr string
+	addr          string
+	cacheType     string
+	cacheCapacity int
 }
 
 func getConfig() *config {
 	cfg := config{}
 	//cfg := new(config)
 	flag.StringVar(&cfg.addr, "addr", ":4000", "http network address")
+	flag.StringVar(&cfg.cacheType, "cacheType", "lfu", "underlying cache type: [lfu, lru, lfu-lru-t]")
+	flag.IntVar(&cfg.cacheCapacity, "cacheCapacity", 100, "cache capacity")
 	flag.Parse()
 	return &cfg
 }
